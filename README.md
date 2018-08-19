c5780f3a3725cc7caa3c39576c13b2c46f812652
Sanjayanbu <snjanbu@gmail.com>
2018-08-19 17:25:11 +0530

    refs #ticket-learning pointcuts and wildcard added

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# SpringAOP
-AOP Implementation 
+# master
--- src/main/aop/aspect/SimpleAspect.java
+++ src/main/aop/aspect/SimpleAspect.java
@@ -2,15 +2,19 @@ package main.aop.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class SimpleAspect {
 	
-	@Before("execution(public String getName())")
+	@Before("allGetters()")
 	public void getLoggingAspect() {
 		System.out.println("Method Execution Starts");
 	}
+	
+	@Pointcut("execution(* get*(..))")
+	public void allGetters() {}
 
 }
